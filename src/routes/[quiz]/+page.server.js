a436a9e5518caba5a7f7f594a99a844df97f8e0e
lepfeiffenberger <90902624+lepfeiffenberger@users.noreply.github.com>
2024-11-15 09:59:35 +0100

    pfeiffenberger

---
+++ src/routes/[quiz]/+page.server.js
@@ -0,0 +1,7 @@
+export async function load({ fetch, params }) {
+	const response = await fetch('data.json');
+	console.log(response);
+	const data = await response.json();
+	const filteredData = data.quizzes.filter((quiz) => quiz.title.toLowerCase() === params.quiz)[0];
+	return filteredData;
+}
---
+++ src/routes/[quiz]/+page.svelte
@@ -0,0 +1,73 @@
+<script>
+	import { page } from '$app/stores';
+	import ListItem from '$lib/components/listItem.svelte';
+	let { data } = $props();
+	let currentQuestion = $state(0);
+	let selectedAnswer = $state(' ');
+	let score = $state(0);
+	let submitted = $state(false);
+
+	function handleSubmit() {
+		data.questions[currentQuestion].selectedAnswer = selectedAnswer;
+		if (selectedAnswer === data.questions[currentQuestion].answer) {
+			score += 1;
+		}
+		submitted = true;
+	}
+
+	function nextQuestion() {
+		currentQuestion++;
+		submitted = false;
+	}
+
+	import { writable } from 'svelte/store';
+
+	const progress = writable(0);
+</script>
+
+<progress value={$progress}></progress>
+{#if currentQuestion < data.questions.length}
+	{#each data.questions as question, index (question.question)}
+		{#if index === currentQuestion}
+			<p>Question {index + 1} of {data.questions.length}</p>
+			<br />
+			<h2>{question.question}</h2>
+			<br />
+			{#each question.options as option, index (option)}
+				{#key submitted}
+					<ListItem
+						title={option}
+						{index}
+						correctAnswer={question.selectedAnswer && question.answer === option}
+						incorrectlySelected={question.selectedAnswer === option && question.answer !== option}
+						focusAction={() => (selectedAnswer = option)}
+						disabled={submitted}
+					></ListItem>
+				{/key}
+			{/each}
+		{/if}
+	{/each}
+
+	<br />
+
+	{#if !submitted}
+		<button class="btn btn-primary" disabled={!selectedAnswer} onclick={handleSubmit}>Submit</button
+		>
+	{:else}
+		<button class="btn btn-primary" onclick={nextQuestion}>Next Question</button>
+	{/if}
+
+	<br />
+	<br />
+	<h1>Punkte: {score}</h1>
+	{selectedAnswer}
+{:else}
+	<h2>Game Over!</h2>
+	<p>You scored {score} out of {data.questions.length} points.</p>
+	<button class="btn btn-primary" onclick={window.location.reload()}>Play again!</button>
+{/if}
+
+<style>
+	progress {
+	}
+</style>
